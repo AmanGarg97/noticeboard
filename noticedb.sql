c94b7bb6e0c26842c9e893474fa8185906ce721b
AmanGarg <amangarg34.ag@gmail.com>
2019-01-21 05:43:26 +0530

    updated Database

--- noticedb.sql
+++ noticedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 13, 2019 at 05:35 PM
+-- Generation Time: Jan 21, 2019 at 12:09 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -35,6 +35,7 @@ CREATE TABLE IF NOT EXISTS `notices` (
   `title` varchar(255) DEFAULT 'NOTICE',
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
+  `imagepath` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -42,13 +43,13 @@ CREATE TABLE IF NOT EXISTS `notices` (
 -- Dumping data for table `notices`
 --
 
-INSERT INTO `notices` (`id`, `notice`, `title`, `createdAt`, `updatedAt`) VALUES
-(1, 'This is sample notice 1', 'NOTICE', '2018-09-10 06:55:43', '2018-09-10 06:55:43'),
-(2, 'This is sample notice 2', 'NOTICE', '2018-09-10 06:55:56', '2018-09-10 06:55:56'),
-(3, 'This is sample notice 3', 'NOTICE', '2018-09-10 06:56:04', '2018-09-10 06:56:04'),
-(4, 'This is sample notice 4', 'NOTICE', '2018-09-10 06:56:12', '2018-09-10 06:56:12'),
-(5, 'In seminar', 'NOTICE', '2018-09-10 06:57:37', '2018-09-10 06:57:37'),
-(6, 'In seminar hall', 'Workshop', '2018-09-10 06:59:34', '2018-09-10 06:59:34');
+INSERT INTO `notices` (`id`, `notice`, `title`, `createdAt`, `updatedAt`, `imagepath`) VALUES
+(1, 'This is sample notice 1', 'NOTICE', '2018-09-10 06:55:43', '2019-01-20 16:59:56', 'public\\uploads\\myImage-1548003596499.jpg'),
+(2, 'This is sample notice 2', 'NOTICE', '2018-09-10 06:55:56', '2019-01-20 16:43:44', ''),
+(3, 'This is sample notice 3', 'NOTICE', '2018-09-10 06:56:04', '2018-09-10 06:56:04', ''),
+(4, 'This is sample notice 4', 'NOTICE', '2018-09-10 06:56:12', '2018-09-10 06:56:12', ''),
+(5, 'In seminar', 'NOTICE', '2018-09-10 06:57:37', '2019-01-20 09:08:38', ''),
+(6, 'In seminar hall', 'Workshop', '2018-09-10 06:59:34', '2019-01-20 09:07:00', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
